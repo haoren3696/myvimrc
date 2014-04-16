6a204d3c821fec4b91b23ec799d98db8e88838f8
haoren3696 <xzp@xzpdeMacBook-Pro.local>
2014-04-17 03:50:12 +0800

    add install plugins

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -16,4 +16,7 @@ endtry' > ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim_runtime/bundle/vundle
 echo "vundle installed"
 
+vim +PluginIstall +qall
+echo "plugins installed"
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
